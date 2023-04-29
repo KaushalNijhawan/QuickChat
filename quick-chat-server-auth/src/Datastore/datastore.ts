be7e301cd5c226ec52b26c5e8acd4d28aa8271e1
KaushalNijhawan <kaushalnijhawan3641@gmail.com>
2023-04-29 21:33:07 +0100

    change

--- quick-chat-server-auth/src/Datastore/datastore.ts
+++ quick-chat-server-auth/src/Datastore/datastore.ts
@@ -62,35 +62,4 @@ export const addUser = async({username , password , email }:{username: string , 
 
       return null;
  }
-=======
-const datastore = new Datastore({
-  projectId: "upbeat-glow-381318",
-  keyFilename: "/Users/air2017/Downloads/superb-cycle.json",
-});
 
-export const addUser = async ({
-  username,
-  password,
-  email,
-}: {
-  username: string;
-  password: string;
-  email: string;
-}) => {
-  const taskKey = datastore.key({ path: ["User"], namespace: "ChatUser" });
-  if (username && password && email) {
-    const task = {
-      key: taskKey,
-      data: {
-        username: username,
-        password: password,
-        email: email,
-      },
-    };
-    let response = await datastore.save(task);
-    console.log(response);
-    return response;
-  }
-
-  return null;
-};
