140e432e8812bdc12f05dc22e4ac61d6954c17cb
ajshukla1902 <130760128+ajshukla1902@users.noreply.github.com>
2023-05-10 20:22:08 +0000

    Changes Merged

--- src/Constants/Constants.ts
+++ src/Constants/Constants.ts
@@ -1,5 +1,5 @@
 export const Constants = {
-    CHAT_MAIN_IP : "localhost",
-    CHAT_AUTH_IP : "localhost",
-    CHAT_FRONTEND_IP : "localhost"
+    CHAT_MAIN_IP : "34.168.174.126",
+    CHAT_AUTH_IP : "35.233.134.93",
+    CHAT_FRONTEND_IP : "34.145.17.169"
 }
